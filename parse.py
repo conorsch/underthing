0f6bf8b6ceebcb9b152bd594bbed7f42e6115f45
Conor Schaefer <conor.schaefer@gmail.com>
2015-11-08 21:06:23 -0800

    Adds function for reading csv file

--- parse.py
+++ parse.py
@@ -7,15 +7,18 @@ filename = 'Data.csv'
 sewer_data = []
 
 
-sewer_data_condensed = []
+def read_csv_file():
+    sewer_data_condensed = []
+    with open(filename, 'rU') as datafile:
+        sewer_reader = csv.reader(datafile, delimiter=',')
+        for row in sewer_reader:
+            sewer_data.append(row)
+            dow, tod, fr = row
+            sewer_data_condensed.append([ ((float(dow) - 1) * 24) + float(tod), fr ])
 
-with open(filename, 'rU') as datafile:
-    sewer_reader = csv.reader(datafile, delimiter=',')
-    for row in sewer_reader:
-        sewer_data.append(row)
-        dow, tod, fr = row
-        sewer_data_condensed.append([ ((float(dow) - 1) * 24) + float(tod), fr ])
+    return sewer_data_condensed
 
+sewer_data_condensed = read_csv_file()
 
 times_of_day = np.array([ np.float(d[0]) for d in sewer_data_condensed ])
 flow_rates = np.array([ np.float(d[1]) for d in sewer_data_condensed ])
