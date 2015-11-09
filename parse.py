a30ba4bbc8bac33abfa30a9305a963319cb7736c
Conor Schaefer <conor.schaefer@gmail.com>
2015-11-08 20:54:49 -0800

    Condensed vectors for time

--- parse.py
+++ parse.py
@@ -6,21 +6,26 @@ import matplotlib.pyplot as plt
 filename = 'Data.csv'
 sewer_data = []
 
+
+sewer_data_condensed = []
+
 with open(filename, 'rU') as datafile:
     sewer_reader = csv.reader(datafile, delimiter=',')
     for row in sewer_reader:
         sewer_data.append(row)
+        dow, tod, fr = row
+        sewer_data_condensed.append([ ((float(dow) - 1) * 24) + float(tod), fr ])
 
-times_of_day = np.array([ np.float(d[1]) for d in sewer_data ])
-flow_rates = np.array([ np.float(d[2]) for d in sewer_data ])
+
+times_of_day = np.array([ np.float(d[0]) for d in sewer_data_condensed ])
+flow_rates = np.array([ np.float(d[1]) for d in sewer_data_condensed ])
 
 coefficients = np.polyfit(times_of_day, flow_rates, 10)
-
 estimate = np.poly1d(coefficients)
 
 x = np.linspace(0, 24, 1000)
-
 _ = plt.plot(times_of_day, flow_rates, x, estimate(x))
 plt.show()
+
 
 
